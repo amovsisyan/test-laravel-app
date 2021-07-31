0e3737765eed4fe5365867ce7386f7a4d6df99c3
art <amovsisyan0@gmail.com>
2021-07-31 23:04:34 +0400

    added relations

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ModelsAdvertisement extends Model
+class Advertisement extends Model
 {
     use HasFactory;
 
     protected $table = 'advertisements';
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class, 'advertisement_id', 'id');
+    }
 }
--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ModelsPhoto extends Model
+class Photo extends Model
 {
     use HasFactory;
 
     protected $table = 'photos';
+
+    public function advertisement()
+    {
+        return $this->hasMany(Advertisement::class, 'advertisement_id', 'id');
+    }
 }
