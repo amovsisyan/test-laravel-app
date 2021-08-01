3b9c0b5608978fec39db3d34ba07237eb53b2da7
art <amovsisyan0@gmail.com>
2021-08-01 20:10:39 +0400

    on delete clean up images

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Advertisement extends Model
 {
@@ -11,6 +12,17 @@ class Advertisement extends Model
 
     protected $table = 'advertisements';
     protected $fillable = ['title', 'description'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($advertisement) {
+            foreach ($advertisement->photos as $photo) {
+                Storage::disk('public')->delete($photo->url); // todo if empty unlink also folder after
+            }
+        });
+    }
 
     public function photos()
     {
